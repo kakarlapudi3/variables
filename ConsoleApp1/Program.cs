7cf817d94589956c895dcfafea1db8912b732037
Ria Kakarlapudi <ria.kakarlapudi@gmail.com>
2024-03-21 20:13:43 -0400

    finished exercise

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string dogName = "Bingo";
+        int dogAge = 5;
+        char exclamation = '!';
+        bool isLab = false;
+        double milesWalked = 2.75;
+        decimal height = 2.5m;
+
+        Console.WriteLine("Enter in your dog's name:");
+        dogName = Console.ReadLine();
+
+        Console.WriteLine(dogName);
+        Console.WriteLine(dogAge);
+
+    }
+
+}
